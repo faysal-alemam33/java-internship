9565e4a18fbe632aa6a185a280b5ede7d18e5f3a
faysal <fesalali3369@gmail.com>
2024-05-02 23:59:31 +0300

    Task 5 Exceptions

---
+++ week-1/src/T1.java
@@ -0,0 +1,13 @@
+public class T1 {
+
+    public static void main(String[] args) {
+
+        int age = 12;
+        double building_high = 16.7;
+        boolean is_it_raining = false;
+        String city_name = "Mansoura";
+
+
+        System.err.println(age + 15);
+    }
+}
